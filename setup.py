12d6c7125c2629db93137e9e4bf1b2cf7a31cc67
Will Nelson <wnelso18@vols.utk.edu>
2023-02-28 10:36:38 -0500

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/wnelso18/wildfly',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
--- wildfly/__init__.py
+++ wildfly/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Will Nelson"""
 __email__ = 'wnelso18@vols.utk.edu'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
